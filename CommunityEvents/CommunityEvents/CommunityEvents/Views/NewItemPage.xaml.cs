d4faf3704618fa2b07bcb57fc2ab5d374d6fe70e
Solaris-Verill <89408538+Solaris-Verill@users.noreply.github.com>
2022-10-03 08:40:43 -0400

    03/10/22

---
+++ CommunityEvents/CommunityEvents/CommunityEvents/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using CommunityEvents.Models;
+using CommunityEvents.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CommunityEvents.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
